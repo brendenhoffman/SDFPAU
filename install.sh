03f745abbe82cffb64ac996830971209a5fe61a8
Brenden Hoffman <13664514+brendenhoffman@users.noreply.github.com>
2022-09-26 18:18:12 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,4 @@
+!#/bin/bash
+wget https://raw.githubusercontent.com/brendenhoffman/SDFPAU/main/SDFPAU.sh -P $HOME/.config/autostart-scripts
+chmod +x $HOME/.config/autostart-scripts/SDFPAU.sh
+rm -f install.sh
