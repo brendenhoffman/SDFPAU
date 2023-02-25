19963529e4322f5bad85e19d35b7730194854d0d
Brenden Hoffman <13664514+brendenhoffman@users.noreply.github.com>
2023-02-24 21:02:24 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 wget https://raw.githubusercontent.com/brendenhoffman/SDFPAU/main/SDFPAU.sh -P $HOME/.config/autostart-scripts
 chmod +x $HOME/.config/autostart-scripts/SDFPAU.sh
 rm -f install.sh
